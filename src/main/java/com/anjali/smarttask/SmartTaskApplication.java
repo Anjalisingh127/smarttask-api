b85371c535e4b12817549cf7f1d6bc4f4ef243b0
Anjalisingh127 <162402705+Anjalisingh127@users.noreply.github.com>
2026-09-19 01:12:28 +0530

    add src/main/java/com/anjali/smarttask/SmartTaskApplication.java

---
+++ src/main/java/com/anjali/smarttask/SmartTaskApplication.java
@@ -0,0 +1,7 @@
+package com.anjali.smarttask;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class SmartTaskApplication {
+    public static void main(String[] args) { SpringApplication.run(SmartTaskApplication.class, args); }
+}
